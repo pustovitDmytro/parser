a532ca66a1610d1554c3f193a3db6aae0f7e4d73
dmytroPustovit <pustic@gmail.com>
2017-05-30 20:48:34 +0300

    last project deleted

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from abc import ABCMeta
 from selenium import webdriver
 import re
 
-BaseUrl ="http://dmsu.gov.ua/online"
-TestUrl = "test/Main.html"
+BaseUrl =""
+TestUrl = "test/"
+
 def main():
-	page = MainPage(BaseUrl)
-	page.ConfirmDay()
+	print("main")
 
 class Page():
 	__metaclass__ = ABCMeta
@@ -21,11 +21,6 @@ class Page():
 	def StartDriver(self):
 		driver = webdriver.Firefox()
 		driver.get(self.link)
-
-		time.sleep(3)
-		organ = driver.find_element_by_id('content')
-		organ.click()
-		time.sleep(1)
 		self.driver = driver
 	def FinishDriver(self):
 		self.driver.close()
@@ -45,8 +40,5 @@ class Page():
 class MainPage(Page):
 	def __init__(self, link, test=False):
 		super(MainPage,self).__init__(link)
-	def ConfirmDay(self):
-		self.StartDriver()
-		driver = self.driver
 if __name__ == '__main__':
     main()
